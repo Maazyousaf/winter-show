44f2e7120596a6ee60fc1c94ceaf3445930116ab
Maaz Yousaf <57700183+Maazyousaf@users.noreply.github.com>
2023-08-10 01:45:29 +0500

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,152 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="max-w-[1512px] mx-auto bg-white">
+      <div className="relative">
+        <img src="./assets/images/1.png" className="w-full h-screen" />
+        <div className="absolute top-1/2 w-full -translate-y-1/2">
+          <h1 className="text-[3.438rem] lg:text-[3.75rem] text-white font-Cormorant-light text-center font-light">
+            WINTER SHOW <span className="block">2023</span>
+          </h1>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-2 h-screen">
+        <img src="./assets/images/2.png" className="w-full h-screen" />
+        <div className="flex justify-center items-center">
+          <h1 className="text-[2.813rem] lg:text-[3.75rem] font-Crimson-Regular-I text-center">
+            MONOCHROME{" "}
+            <span className="block font-Crimson-Regular">AFFAIR</span>
+          </h1>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-2 h-screen">
+        <div className="flex justify-center flex-col items-center">
+          <h1 className="lg:text-[5rem] text-[2.813rem] font-Crimson-Regular text-center">
+            STYLE
+          </h1>
+          <p className="font-Noto w-[270px] font-light leading-relaxed text-justify text-lg lg:text-xl">
+            Step into the world of Luceria & Co. and immerse yourself in the
+            allure of masterful craftsmanship and timeless beauty.{" "}
+          </p>
+        </div>
+        <img src="./assets/images/3.png" className="w-full h-screen" />
+      </div>
+
+      <div className="grid grid-cols-2 h-screen">
+        <img src="./assets/images/4.png" className="w-full h-screen" />
+        <div className="flex justify-center items-center">
+          <h1 className="text-3xl lg:text-[3.125rem] font-Crimson-Regular text-center px-12">
+            “<span className=" font-Crimson-Regular-I">Elegance</span> is not
+            standing out, but being{" "}
+            <span className=" font-Crimson-Regular-I">remembered.</span>” -
+            <span className="text-2xl lg:text-[2.5rem] block">
+              {" "}
+              Giorgio Armani
+            </span>
+          </h1>
+        </div>
+      </div>
+
+      <img src="./assets/images/5.png" className="w-full h-screen" />
+
+      <div className="grid grid-cols-2 h-screen">
+        <div className="flex justify-center flex-col items-center">
+          <h1 className="text-[3.75rem] lg:text-[5rem] font-Crimson-Regular text-center">
+            EMOTION
+          </h1>
+          <p className="font-Noto w-[270px] font-light lg:leading-relaxed text-justify text-lg lg:text-xl leading-[35px]">
+            Every thread tells a story, every stitch is a note in the melody of
+            the designer's narrative. From the grandeur of haute couture to the
+            avant-garde allure of experimental creations, fashion becomes a
+            living embodiment of the artist's soul, a mirror reflecting the
+            ever-evolving society it adorns.
+          </p>
+        </div>
+        <img src="./assets/images/6.png" className="w-full h-screen" />
+      </div>
+
+      <div className="grid grid-cols-2 h-screen">
+        <img src="./assets/images/7.png" className="w-full h-screen" />
+        <div className="flex justify-center items-center px-8 md:px-16">
+          <h1 className="text-3xl lg:text-[3.125rem] font-Crimson-Regular px-1 text-center">
+            “<span className=" font-Crimson-Regular-I">Fashion</span>
+            is a form of{" "}
+            <span className=" font-Crimson-Regular-I">escapism</span> not an
+            imprisonment.” -{" "}
+            <span className="text-2xl lg:text-[2.5rem] block">
+              Alexander Mcqueen
+            </span>
+          </h1>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-2 h-screen">
+        <div className="flex justify-center flex-col items-center">
+          <p className="font-Noto w-[270px] font-light leading-relaxed text-justify text-lg lg:text-xl">
+            Embrace every moment with the self-assured grace that defines you,
+            and watch as your style radiates a brilliance matched only by your
+            confidence.
+          </p>
+        </div>
+        <img src="./assets/images/8.png" className="w-full h-screen" />
+      </div>
+
+      <img src="./assets/images/9.png" className="w-full h-screen" />
+
+      <div className="grid grid-cols-2 h-screen">
+        <img src="./assets/images/10.png" className="w-full h-screen" />
+        <div className="flex justify-center items-center">
+          <h2 className="text-[3.75rem] lg:text-[5rem] font-Crimson-Regular text-center w-[250px] lg:w-[300px] leading-[60px]">
+            let the <span className="font-Crimson-Regular-I">world</span> be{" "}
+            your <span className="font-Crimson-Regular-I">runway</span>
+          </h2>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-2 h-screen">
+        <div className="flex justify-center items-center">
+          <h2 className="text-[3.75rem] font-Cormorant-light text-center leading-[60px] font-light">
+            Runway 20 <span className="block">11/02/2023</span>
+          </h2>
+        </div>
+        <img src="./assets/images/11.png" className="w-full h-screen" />
+      </div>
+
+      <div className="grid grid-cols-2 h-screen">
+        <img src="./assets/images/12.png" className="w-full h-screen" />
+        <div className="flex justify-center items-center">
+          <div className="w-[15rem] lg:w-[18.75rem] flex flex-col gap-y-4">
+            <div className="w-full font-Montserrat !font-semibold text-xl h-14 border border-black flex items-center justify-center">
+              SHARE PAGE
+            </div>
+            <div className="w-full font-Montserrat font-semibold text-xl h-14 border border-black flex items-center justify-center">
+              OFFICIAL WEBSITE
+            </div>
+            <div className="w-full font-Montserrat font-semibold text-xl h-14 border border-black flex items-center justify-center">
+              RESERVE
+            </div>
+            <div className="w-full font-Montserrat font-semibold text-xl h-14 border border-black flex items-center justify-center">
+              EMAIL
+            </div>
+            <div className="w-full font-Montserrat font-semibold text-xl h-14 border border-black flex items-center justify-center">
+              PHONE
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="relative">
+        <img src="./assets/images/13.png" className="w-full h-screen" />
+        <div className="absolute w-full top-1/2 -translate-y-1/2">
+          <h1 className="text-[3.75rem] lg:text-[4rem] text-white font-Crimson-Regular-I text-center font-light">
+            Chapter <span className="Crimson-Regular">3 is COMING.</span>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
